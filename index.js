eb1d34aaa99b74b382f5f3ead5a199f18f9bd409
emilytm <emilytmcintyre@gmail.com>
2022-10-31 11:06:24 -0400

    got link replacement to work

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
 const lightning = "lightning.force."
 const classic = "my.salesforce."
 
+function findlinks() {
+    for (const link of document.links) {
+        if(link.href.includes(lightning)) {
+            let betterLink = link.href.replace(lightning, classic)
+            link.setAttribute('href',betterLink)
+        }
+    }
+}
