92192d72efedfbc652e8a9c25bd55f23618a9aa1
emilytm <emilytmcintyre@gmail.com>
2022-11-28 10:42:50 -0500

    Learned about query selectors and made it work!

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const classic = "my.salesforce."
 //btnEl.addEventListener("click",fixlinks())
 
 //document.addEventListener('DOMContentLoaded', fixlinks())
-
+fixlinks()
 function fixlinks() {
-    for (const link of document.links) {
+    /*for (const link of document.links) {
         if(link.href.includes(lightning)) {
             let betterLink = link.href.replace(lightning, classic)
             link.setAttribute('href',betterLink)
         }
     }
+    */
+   const linksList = document.querySelectorAll("a[href*='lightning.force.']")
+   for (let badLink of linksList) {
+        var replaceLink = badLink.href.replace('lightning.force.', 'my.salesforce.')
+        badLink.href = replaceLink
+   }
 }
 //Got the code from here down from https://stackoverflow.com/questions/4842590/how-to-run-a-function-when-the-page-is-loaded
 
